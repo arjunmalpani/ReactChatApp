6cd32c9ed8f49099143e90ac353b5727feecc218
arjunmalpani <a4arjunmalpani@gmail.com>
2025-07-03 17:10:10 +0530

    path fix omg

--- server/src/index.js
+++ server/src/index.js
@@ -1,43 +1,46 @@
-import { configDotenv } from 'dotenv';
-configDotenv()
-import express from 'express';
-import authRoutes from './routes/auth.route.js';
-import messageRoutes from './routes/message.route.js';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import { connectDB } from './lib/db.js';
-import { app , server , io } from './lib/socket.js';
-import path from 'path';
-const PORT = process.env.PORT
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
+import path from "path";
 
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
-// Middlewares
-app.use(cookieParser())
-app.use(express.json({ limit: '10mb' })) // Increase JSON payload limit
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+console.log(__dirname);
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true, // Allow cookies to be sent
-}))
-// Routes
-app.use((req, res, next) => {
-    console.log(`[${req.method}] ${req.url}`);
-    next();
-});
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
 
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-app.use('/api/auth', authRoutes)
-app.use('/api/message', messageRoutes)
+if (process.env.NODE_ENV === "production") {
+    const staticPath = path.join(__dirname, "../client", "dist");
+    const indexPath = path.join(staticPath, "index.html");
+    app.use(express.static(staticPath));
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+    app.get("/*path", (req, res) => {
+        res.sendFile(indexPath);
     });
+
 }
+
 server.listen(PORT, () => {
-    console.log('Server is running at PORT:', PORT);
-    connectDB()
-})
+    console.log("server is running on PORT:" + PORT);
+    connectDB();
+});
