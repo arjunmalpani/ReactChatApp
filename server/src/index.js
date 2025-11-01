a1df6b5d004aa31e12ba4f52247c430a64594cb1
Arjun <160125274+arjunmalpani@users.noreply.github.com>
2025-11-01 16:56:52 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -18,7 +18,7 @@ const __dirname = path.resolve();
 console.log(__dirname);
 
 app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
     cors({
